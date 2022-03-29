ad7bae5aca66c9a90a1262dc32617cca868eb3e3
Andrew Sidorchuk <andrew.sidorchuk@lifeway.com>
2022-03-29 00:20:12 -0400

    Started adding auto scroll

---
+++ src/components/SongToolsBottomSheet.js
@@ -0,0 +1,60 @@
+import React, {useEffect, useRef} from 'react';
+
+import BottomSheet from './BottomSheet';
+import FontBottomSheetScreen from '../screens/FontBottomSheetScreen';
+import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {useTheme} from '../hooks/useTheme';
+import SongToolsBottomSheetScreen from '../screens/SongToolsBottomSheetScreen';
+import AutoscrollBottomSheetScreen from '../screens/AutoscrollBottomSheetScreen';
+
+const Stack = createNativeStackNavigator();
+export default function SongToolsBottomSheet({visible, onDismiss}) {
+  const sheetRef = useRef();
+  const {isDark, surface, text} = useTheme();
+
+  useEffect(() => {
+    if (visible) sheetRef.current?.present();
+  }, [visible, sheetRef]);
+
+  return (
+    <BottomSheet
+      snapPoints={['20%', '80%']}
+      onDismiss={onDismiss}
+      ref={sheetRef}
+      snapIndex={1}>
+      <NavigationContainer
+        independent
+        theme={{
+          ...DefaultTheme,
+          colors: {
+            ...DefaultTheme.colors,
+            background: isDark
+              ? surface.secondary.backgroundColor
+              : surface.primary.backgroundColor,
+          },
+        }}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="SongTools Menu"
+            component={SongToolsBottomSheetScreen}
+            options={() => ({
+              headerShown: false,
+            })}
+          />
+          <Stack.Screen
+            name="Autoscroll"
+            component={AutoscrollBottomSheetScreen}
+            options={() => ({
+              title: 'Font',
+              headerShadowVisible: false,
+              headerTitleAlign: 'left',
+              headerStyle: isDark ? surface.secondary : surface.primary,
+              headerTitleStyle: text.primary,
+            })}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </BottomSheet>
+  );
+}
---
+++ src/screens/SongToolsBottomSheetScreen.js
@@ -0,0 +1,44 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {ScrollView} from 'react-native-gesture-handler';
+import RectButton from '../components/RectButton';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {useTheme} from '../hooks/useTheme';
+
+export default function SongToolsBottomSheetScreen({navigation}) {
+  const {blue, text} = useTheme();
+
+  return (
+    <ScrollView contentContainerStyle={[styles.screen]}>
+      <RectButton
+        styles={styles.toolButton}
+        onPress={() => navigation.push('Autoscroll')}>
+        <View style={styles.toolLeftContainer}>
+          <Icon name="mouse" size={22} color={blue.text.color} />
+          <Text style={[styles.buttonText, text.primary]}>Autoscroll</Text>
+        </View>
+        <Icon name="chevron-right" size={18} color={text.primary.color} />
+      </RectButton>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+  },
+  toolButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  toolLeftContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontWeight: '600',
+    marginLeft: 15,
+  },
+});
